052699bfcddee27bfd4f9395ce498f24773190fa
bobinlee <bobeen.lee@huiseoul.com>
2022-05-27 21:03:22 +0900

    feat: add post view pages

--- apps/blog-remix/app/models/post.server.ts
+++ apps/blog-remix/app/models/post.server.ts
@@ -1,5 +1,11 @@
 import { prisma } from "~/libs/prisma.server";
 
+export type { Post } from "~/generated";
+
 export async function getPosts() {
     return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+    return prisma.post.findUnique({ where: { slug } });
 }
--- apps/blog-remix/app/routes/posts/$slug.tsx
+++ apps/blog-remix/app/routes/posts/$slug.tsx
@@ -1,17 +1,32 @@
 import { json, LoaderFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { marked } from "marked";
+import invariant from 'tiny-invariant';
+
+import { getPost, Post } from '~/models/post.server';
+
+interface LoaderData {
+  post: Awaited<ReturnType<typeof getPost>>;
+  html: string;
+}
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ slug: params.slug });
+  invariant(params.slug, `params.slug is required`);
+  const post = await getPost(params.slug ?? '');
+  invariant(post, `Post not found: ${params.slug}`);
+  
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { slug } = useLoaderData();
+  const { post, html } = useLoaderData<LoaderData>();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
+        {post?.title ?? ''}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
