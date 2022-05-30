93ce3ee79c1fa8ec5895735a4e5d04b1a4e328d1
bobinlee <bobeen.lee@huiseoul.com>
2022-05-30 23:17:42 +0900

    fix: remove console

--- apps/blog-remix/app/routes/jokes.tsx
+++ apps/blog-remix/app/routes/jokes.tsx
@@ -26,7 +26,6 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function JokesRoute() {
   const data = useLoaderData<LoaderData>();
-  console.log(data);
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
